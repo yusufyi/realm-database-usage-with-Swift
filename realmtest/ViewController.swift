5b391c1fcd5fd462191e5f31816c2ed1b4a0f113
yusuf <yusufyi@hotmail.com>
2018-12-22 10:05:06 +0300

    Initial Commit

---
+++ realmtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realmtest
+//
+//  Created by Yusuf Yigit on 22.12.2018.
+//  Copyright © 2018 Yusuf Yigit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
